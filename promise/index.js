7456b4b7743ddf977373fba741b73fc0dd69ac81
GAtomis <850680822@qq.com>
2023-03-09 20:54:11 +0800

    update : promise

--- promise/index.js
+++ promise/index.js
@@ -1,11 +1,27 @@
 var APromise = /** @class */ (function () {
     function APromise(executor) {
-        // 初始化state为等待态
-        this.state = 'pending';
-        // 成功的值
-        this.value = undefined;
-        // 失败的原因
-        this.reason = undefined;
+        var _this = this;
+        this.resolve = function (value) {
+            // state改变,resolve调用就会失败    
+            if (_this.state === APromise.PENDING) {
+                // resolve调用后，state转化为成功态
+                _this.state = APromise.FULFILLED;
+                // 储存成功的值
+                _this.value = value;
+                _this.onResolvedCallbacks.forEach(function (fn) { return fn(); });
+            }
+        };
+        this.reject = function (reason) {
+            // state改变,reject调用就会失败
+            if (_this.state === APromise.PENDING) {
+                // reject调用后，state转化为失败态
+                _this.state = APromise.REJECTED;
+                // 储存失败的原因
+                _this.reason = reason;
+                _this.onRejectedCallbacks.forEach(function (fn) { return fn(); });
+            }
+        };
+        this.initVal();
         // 如果executor执行报错，直接执行reject
         try {
             executor(this.resolve, this.reject);
@@ -14,43 +30,54 @@ var APromise = /** @class */ (function () {
             this.reject(err);
         }
     }
-    APromise.prototype.resolve = function (value) {
-        // state改变,resolve调用就会失败
-        if (this.state === 'pending') {
-            // resolve调用后，state转化为成功态
-            this.state = 'fulfilled';
-            // 储存成功的值
-            this.value = value;
-        }
-    };
-    ;
-    APromise.prototype.reject = function (reason) {
-        // state改变,reject调用就会失败
-        if (this.state === 'pending') {
-            // reject调用后，state转化为失败态
-            this.state = 'rejected';
-            // 储存失败的原因
-            this.reason = reason;
-        }
+    //初始化绑定
+    APromise.prototype.initVal = function () {
+        // 初始化state为等待态
+        this.state = APromise.PENDING;
+        // 成功的值
+        this.value = undefined;
+        // 失败的原因
+        this.reason = undefined;
+        this.onRejectedCallbacks = [];
+        this.onResolvedCallbacks = [];
     };
     APromise.prototype.then = function (onFulfilled, onRejected) {
-        if (this.isFunction(onFulfilled)) {
+        var _this = this;
+        if (onFulfilled && !this.isFunction(onFulfilled))
+            throw new Error("请传入正确的方法");
+        if (onRejected && !this.isFunction(onRejected))
+            throw new Error("请传入正确的方法");
+        //命中状态
+        if (this.state == APromise.FULFILLED) {
+            onFulfilled === null || onFulfilled === void 0 ? void 0 : onFulfilled(this.value);
         }
-        else {
-            new Error("请传入正确的方法");
+        //拒绝状态
+        if (this.state == APromise.REJECTED) {
+            onRejected === null || onRejected === void 0 ? void 0 : onRejected(this.reason);
         }
-        if (this.isFunction(onRejected)) {
+        if (this.state == APromise.PENDING) {
+            console.log(this.onResolvedCallbacks);
+            this.onResolvedCallbacks.push(function () { return onFulfilled(_this.value); });
+            // this.onRejectedCallbacks.push(onRejected)
         }
     };
     APromise.prototype.isFunction = function (val) {
         if (!val)
-            new Error("请输入参数");
+            throw new Error("请输入参数");
         return typeof val == "function";
+    };
+    APromise.PENDING = "pending";
+    APromise.FULFILLED = "fulfilled";
+    APromise.REJECTED = "rejected";
+    APromise.resolvePromise = function (promise2, x, resolve, reject) {
     };
     return APromise;
 }());
 new APromise(function (resolve, reject) {
     setTimeout(function () {
         resolve(1);
-    });
-});
+    }, 1000);
+}).then(function (res) {
+    console.log(res);
+    return 1 + res;
+}).then();
--- promise/index.ts
+++ promise/index.ts
@@ -4,60 +4,78 @@ class APromise<T> {
     private reason: any
     private onResolvedCallbacks: any[]
     private onRejectedCallbacks: any[]
+    static PENDING: "pending" = "pending"
+    static FULFILLED: "fulfilled" = "fulfilled"
+    static REJECTED: "rejected" = "rejected"
+    static resolvePromise=function(promise2:APromise<any>,x,resolve,reject){
+
+    }
     constructor(executor) {
-        // 初始化state为等待态
-        this.state = 'pending';
-        // 成功的值
-        this.value = undefined;
-        // 失败的原因
-        this.reason = undefined;
-    
+        this.initVal()
         // 如果executor执行报错，直接执行reject
         try {
             executor(this.resolve, this.reject);
         } catch (err) {
-           this.reject(err);
+            this.reject(err);
         }
     }
-    resolve(value) {
-        // state改变,resolve调用就会失败
-        if (this.state === 'pending') {
+    //初始化绑定
+    initVal(){
+        // 初始化state为等待态
+        this.state = APromise.PENDING;
+        // 成功的值
+        this.value = undefined;
+        // 失败的原因
+        this.reason = undefined;
+        this.onRejectedCallbacks=[]
+        this.onResolvedCallbacks=[]
+    }
+    resolve =(value) =>{
+        // state改变,resolve调用就会失败    
+
+        if (this.state === APromise.PENDING) {
             // resolve调用后，state转化为成功态
-            this.state = 'fulfilled';
+            this.state = APromise.FULFILLED;
             // 储存成功的值
             this.value = value;
+            this.onResolvedCallbacks.forEach(fn=>fn())
+
         }
-    };
-    reject(reason) {
+    }
+    reject = (reason)=> {
         // state改变,reject调用就会失败
-        if (this.state === 'pending') {
+        if (this.state === APromise.PENDING) {
             // reject调用后，state转化为失败态
-            this.state = 'rejected';
+            this.state = APromise.REJECTED;
             // 储存失败的原因
             this.reason = reason;
+            this.onRejectedCallbacks.forEach(fn=>fn())
         }
     }
 
-    then(onFulfilled, onRejected) {
-
-        if(this.isFunction(onFulfilled)){
-
-
-        }else{
-            new Error("请传入正确的方法")
+    then (onFulfilled, onRejected?) {
+        if (onFulfilled && !this.isFunction(onFulfilled)) throw new Error("请传入正确的方法")
+        if (onRejected && !this.isFunction(onRejected)) throw new Error("请传入正确的方法")
+        //命中状态
+        if (this.state == APromise.FULFILLED) {
+            onFulfilled?.(this.value)
         }
-        if(this.isFunction(onRejected)){
-
-
+        //拒绝状态
+        if (this.state == APromise.REJECTED) {
+            onRejected?.(this.reason)
         }
-        
-
+        if (this.state==APromise.PENDING){
+            console.log(this.onResolvedCallbacks);
+            
+            this.onResolvedCallbacks.push(()=>onFulfilled(this.value))
+            // this.onRejectedCallbacks.push(onRejected)
+        }
     }
-    isFunction(val:any){
-        if (!val) new Error("请输入参数")
-        return typeof val=="function"
+    private isFunction(val: any) {
+        if (!val) throw new Error("请输入参数")
+        return typeof val == "function"
 
-     
+
     }
 
 
@@ -65,10 +83,15 @@ class APromise<T> {
 }
 
 
-new APromise((resolve, reject) => {    
+new APromise((resolve, reject) => {
+
     setTimeout(() => {
-
         resolve(1)
-    })
+    },1000)
 
-})
+}).then(res => {
+    console.log(res);
+
+    return 1+res
+
+}).then()
